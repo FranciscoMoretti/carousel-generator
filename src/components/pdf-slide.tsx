0d0387762b57ce48c0fbdbd70aac94c15281557b
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-22 09:06:05 +0100

    simplfied pdfslide

--- src/components/pdf-slide.tsx
+++ src/components/pdf-slide.tsx
@@ -43,10 +43,10 @@ export function PdfSlide({
         size="A6"
         style={{
           backgroundColor: theme.accent,
-          ...tw("  p-8 flex flex-col justify-between"),
+          ...tw("p-8 flex flex-col justify-between"),
         }}
       >
-        <View style={tw("flex flex-col  p-6")}>
+        <View style={tw("flex flex-col p-6")}>
           <Text
             style={{
               color: theme.primary,
@@ -58,7 +58,7 @@ export function PdfSlide({
           <Text
             style={{
               color: theme.secondary,
-              ...tw("text-sm text-muted-foreground"),
+              ...tw("text-sm"),
             }}
           >
             {slide.subtitle}
@@ -66,51 +66,5 @@ export function PdfSlide({
         </View>
       </Page>
     </Document>
-  );
-}
-
-export function CarouselSlide({
-  slide,
-  settings,
-  theme,
-}: {
-  slide: z.infer<typeof SlideSchema>;
-  settings: z.infer<typeof SettingsSchema>;
-  theme: z.infer<typeof ThemeSchema>;
-}) {
-  return (
-    <Card
-      className="w-[448px] h-[560px] aspect-[4/5] p-8 flex flex-col justify-between"
-      style={{ backgroundColor: theme.accent }}
-    >
-      <CardHeader>
-        <CardTitle style={{ color: theme.primary }}>{slide.title}</CardTitle>
-        <CardDescription style={{ color: theme.secondary }}>
-          {slide.subtitle}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <span className="">{slide.description}</span>
-      </CardContent>
-      <CardFooter
-        style={{ color: theme.primary }}
-        className="flex justify-start gap-3 "
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={settings.avatar}
-          alt="Avatar"
-          className="w-12 h-12 rounded-full"
-        />
-        <div className="flex flex-col">
-          <span className="" style={{ color: theme.primary }}>
-            {settings.name}
-          </span>
-          <span className="" style={{ color: theme.secondary }}>
-            {settings.handle}
-          </span>
-        </div>
-      </CardFooter>
-    </Card>
   );
 }
