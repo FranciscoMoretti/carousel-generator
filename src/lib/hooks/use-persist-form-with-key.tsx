39f9fa4c01dfd1fc66109f6898ed6e91ff9096b5
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-22 08:31:28 +0100

    fixed window usage

--- src/lib/hooks/use-persist-form-with-key.tsx
+++ src/lib/hooks/use-persist-form-with-key.tsx
@@ -3,9 +3,11 @@ import useFormPersist from "react-hook-form-persist";
 export function usePersistFormWithKey(settingsForm: any, key: string) {
   const { watch: settingsWatch, setValue: settingsSetValue } = settingsForm;
 
+  const localStorage =
+    typeof window !== "undefined" ? window.localStorage : undefined;
   useFormPersist(key, {
     watch: settingsWatch,
     setValue: settingsSetValue,
-    storage: window.localStorage,
+    storage: localStorage,
   });
 }
