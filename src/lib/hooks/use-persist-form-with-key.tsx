78a6c3b80fa96ceddc434fa063abc44f7a25038e
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-23 11:18:48 +0100

    update persistform variables

--- src/lib/hooks/use-persist-form-with-key.tsx
+++ src/lib/hooks/use-persist-form-with-key.tsx
@@ -1,13 +1,13 @@
 import useFormPersist from "react-hook-form-persist";
 
 export function usePersistFormWithKey(settingsForm: any, key: string) {
-  const { watch: settingsWatch, setValue: settingsSetValue } = settingsForm;
+  const { watch, setValue } = settingsForm;
 
   const localStorage =
     typeof window !== "undefined" ? window.localStorage : undefined;
   useFormPersist(key, {
-    watch: settingsWatch,
-    setValue: settingsSetValue,
+    watch,
+    setValue,
     storage: localStorage,
   });
 }
