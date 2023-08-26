3352c818d7a593a14d508ce8b1d7591bcbfc002b
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-26 11:27:58 +0100

    structure for tabs

--- src/components/slide-panel.tsx
+++ src/components/slide-panel.tsx
@@ -1,38 +1,58 @@
-import { useState } from "react";
-import { SidebarMenu } from "./menu-bar";
 import { SlidesForm } from "./slides-form";
 import { SettingsForm } from "./settings-form";
 import { ThemeForm } from "./theme-form";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
-const ALL_FORMS = ["slide", "settings", "theme"];
+const ALL_FORMS = {
+  slide: {
+    name: "Slide",
+    value: "slide",
+  },
+  settings: {
+    name: "Settings",
+    value: "settings",
+  },
+  theme: {
+    name: "Theme",
+    value: "theme",
+  },
+};
 
 export function SlidePanel({ currentSlide }: { currentSlide: number }) {
   return (
     <Tabs defaultValue="slide" className="flex-1">
       <div className="flex flex-col gap-6 w-[448px] h-[560px] ">
         <TabsList className="grid grid-cols-3 shadow">
-          <TabsTrigger value="slide">
-            <span className="sr-only">Slide</span>
-            Slide
+          <TabsTrigger value={ALL_FORMS.slide.value}>
+            <span className="sr-only">{ALL_FORMS.slide.name}</span>
+            {ALL_FORMS.slide.name}
           </TabsTrigger>
-          <TabsTrigger value="settings">
-            <span className="sr-only">Settings</span>
-            Settings
+          <TabsTrigger value={ALL_FORMS.settings.value}>
+            <span className="sr-only">{ALL_FORMS.settings.name}</span>
+            {ALL_FORMS.settings.name}
           </TabsTrigger>
-          <TabsTrigger value="theme">
-            <span className="sr-only">Theme</span>
-            Theme
+          <TabsTrigger value={ALL_FORMS.theme.value}>
+            <span className="sr-only">{ALL_FORMS.theme.name}</span>
+            {ALL_FORMS.theme.name}
           </TabsTrigger>
         </TabsList>
         <div className="border p-4 rounded  shadow">
-          <TabsContent value="slide" className="mt-0 border-0 p-0 ">
+          <TabsContent
+            value={ALL_FORMS.slide.value}
+            className="mt-0 border-0 p-0 "
+          >
             <SlidesForm currentSlide={currentSlide} />
           </TabsContent>
-          <TabsContent value="settings" className="mt-0 border-0 p-0">
+          <TabsContent
+            value={ALL_FORMS.settings.value}
+            className="mt-0 border-0 p-0"
+          >
             <SettingsForm />
           </TabsContent>
-          <TabsContent value="theme" className="mt-0 border-0 p-0">
+          <TabsContent
+            value={ALL_FORMS.theme.value}
+            className="mt-0 border-0 p-0"
+          >
             <ThemeForm />
           </TabsContent>
         </div>
