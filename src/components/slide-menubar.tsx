9aaca9e2afab6500a4681dda3ebb898457f66097
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-27 09:44:01 +0100

    reorder controls

--- src/components/slide-menubar.tsx
+++ src/components/slide-menubar.tsx
@@ -1,15 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { usePagerContext } from "@/lib/providers/pager-context";
+import { DocumentSchema } from "@/lib/validation/document-schema";
+import { UseFormReturn, useFieldArray, useFormContext } from "react-hook-form";
+import * as z from "zod";
 
 interface Props extends React.HTMLAttributes<HTMLElement> {}
 
 export default function SlideMenubar({}: Props) {
   const { currentPage, numPages } = usePagerContext();
+  const {
+    control,
+  }: UseFormReturn<
+    z.infer<typeof DocumentSchema>,
+    any,
+    undefined
+  > = useFormContext(); // retrieve those props
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
+    {
+      control, // control props comes from useForm (optional: if you are using FormContext)
+      name: "slides", // unique name for your Field Array
+    }
+  );
 
   return (
     <div className="flex flex-row gap-1">
       <Button
-        onClick={() => null}
+        onClick={() => swap(currentPage, currentPage - 1)}
         variant="outline"
         size="sm"
         disabled={currentPage == 0}
@@ -23,7 +39,7 @@ export default function SlideMenubar({}: Props) {
         Delete
       </Button>
       <Button
-        onClick={() => null}
+        onClick={() => swap(currentPage, currentPage + 1)}
         variant="outline"
         size="sm"
         disabled={currentPage == numPages - 1}
