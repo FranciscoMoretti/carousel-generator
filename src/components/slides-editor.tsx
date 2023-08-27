33c888f7d9ed863f7161483067f10f76cbd9d6ee
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-27 19:59:41 +0100

    reduced gap

--- src/components/slides-editor.tsx
+++ src/components/slides-editor.tsx
@@ -1,9 +1,5 @@
-import { useState } from "react";
 import PDFViewer from "./PDFViewer";
 import { SlidePanel } from "./slide-panel";
-import { EditorMenubar } from "./editor-menubar";
-import { usePager } from "@/lib/hooks/use-pager";
-import { usePagerContext } from "@/lib/providers/pager-context";
 import SlideMenubar from "./slide-menubar";
 
 interface SlidesEditorProps {
@@ -14,7 +10,7 @@ interface SlidesEditorProps {
 export function SlidesEditor({ isntanceUrl, length }: SlidesEditorProps) {
   return (
     <div className="flex flex-col w-full items-center justify-start gap-4 p-4">
-      <div className=" flex flex-col p-4 w-full items-center justify-start gap-4 rounded font-mono text-sm border shadow">
+      <div className=" flex flex-col p-4 w-full items-center justify-start gap-2 rounded font-mono text-sm border shadow">
         {/* React Slide for debug purposes */}
         {/* <div className="border p-4 rounded shadow flex flex-col items-center ">
         <CarouselSlide
