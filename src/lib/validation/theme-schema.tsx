83a8ecd795128e388177f97b4272e43ba3bff071
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-29 09:34:02 +0100

    simple validation in color schema

--- src/lib/validation/theme-schema.tsx
+++ src/lib/validation/theme-schema.tsx
@@ -1,7 +1,8 @@
 import * as z from "zod";
 
 export const ThemeSchema = z.object({
-  primary: z.string(),
+  // primary: z.string().min(4).max(9).regex(/^#/),
+  primary: z.string().min(7).max(7).regex(/^#/),
   secondary: z.string(),
   background: z.string(),
 });
