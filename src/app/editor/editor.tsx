10690bff062c91c909b549483efb2288e227b29b
Francisco Moretti <franciscoemoretti@gmail.com>
2023-09-05 11:09:11 +0100

    removed unused imports

--- src/app/editor/editor.tsx
+++ src/app/editor/editor.tsx
@@ -22,18 +22,6 @@ import React from "react";
 import { useReactToPrint } from "react-to-print";
 import { SIZE } from "@/lib/pdf-resources";
 
-// import { CodeViewer } from "./components/code-viewer";
-// import { MaxLengthSelector } from "./components/maxlength-selector";
-// import { ModelSelector } from "./components/model-selector";
-// import { PresetActions } from "./components/preset-actions";
-// import { PresetSave } from "./components/preset-save";
-// import { PresetSelector } from "./components/preset-selector";
-// import { PresetShare } from "./components/preset-share";
-// import { TemperatureSelector } from "./components/temperature-selector";
-// import { TopPSelector } from "./components/top-p-selector";
-// import { models, types } from "./data/models";
-// import { presets } from "./data/presets";
-
 export const metadata: Metadata = {
   title: "Playground",
   description: "The OpenAI Playground built using the components.",
