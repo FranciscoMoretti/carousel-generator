1c36f76537b0d82828196e2e2ff8567639b459df
Francisco Moretti <franciscoemoretti@gmail.com>
2023-09-12 12:11:27 +0100

    solved menubar

--- src/components/editor-menubar.tsx
+++ src/components/editor-menubar.tsx
@@ -4,7 +4,7 @@ import { useFormContext } from "react-hook-form";
 import { Button } from "./ui/button";
 import { DocumentFormReturn } from "@/lib/document-form-types";
 import { Loader2Icon } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { JsonExporter } from "./json-exporter";
 import { JsonImporter } from "./json-importer";
 import {
@@ -16,6 +16,15 @@ import {
   MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import FileInputForm from "./file-input-form";
+import { useFieldsFileImporter } from "@/lib/hooks/use-fields-file-importer";
 
 export function EditorMenubar({
   handlePrint,
@@ -25,10 +34,15 @@ export function EditorMenubar({
   isPrinting: boolean;
 }) {
   const { reset, watch }: DocumentFormReturn = useFormContext(); // retrieve those props
+  const [isConfigDialogOpen, setIsConfigDialogOpen] = useState(false);
+  const { handleFileSubmission: handleConfigFileSubmission } =
+    useFieldsFileImporter("config");
+  const [isContentDialogOpen, setIsContentDialogOpen] = useState(false);
+
+  const { handleFileSubmission: handleContentFileSubmission } =
+    useFieldsFileImporter("slides");
 
   // TODO: Show filename form in menubar
-  // TODO: Move import/export to menubar File menu
-  // TODO: Refactor import/export
   return (
     <div className="ml-auto flex w-full gap-2 items-center flex-col-reverse md:flex-row lg:justify-between md:justify-center px-6 flex-wrap">
       <div className="flex flex-row gap-2 ">
@@ -38,9 +52,9 @@ export function EditorMenubar({
             <MenubarContent>
               <JsonExporter
                 values={watch("config")}
-                filename={"carousel-config.json"}
+                filename={"carousel-settings.json"}
               >
-                <MenubarItem>Export Config</MenubarItem>
+                <MenubarItem>Export Settings</MenubarItem>
               </JsonExporter>
               <JsonExporter
                 values={watch("slides")}
@@ -49,6 +63,56 @@ export function EditorMenubar({
                 <MenubarItem>Export Content</MenubarItem>
               </JsonExporter>
               <MenubarSeparator />
+              <Dialog
+                open={isContentDialogOpen}
+                onOpenChange={setIsContentDialogOpen}
+              >
+                <DialogTrigger asChild>
+                  <MenubarItem onSelect={(e) => e.preventDefault()}>
+                    Import Content
+                  </MenubarItem>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Load a file with content</DialogTitle>
+                  </DialogHeader>
+
+                  <FileInputForm
+                    handleSubmit={(files) => {
+                      handleContentFileSubmission(files);
+                      setIsContentDialogOpen(false);
+                    }}
+                    label={"Content File"}
+                    description="Select a json file to load"
+                  />
+                </DialogContent>
+              </Dialog>
+              <Dialog
+                open={isConfigDialogOpen}
+                onOpenChange={setIsConfigDialogOpen}
+              >
+                <DialogTrigger asChild>
+                  <MenubarItem onSelect={(e) => e.preventDefault()}>
+                    Import Settings
+                  </MenubarItem>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Load a file with Settings</DialogTitle>
+                  </DialogHeader>
+
+                  <FileInputForm
+                    handleSubmit={(files) => {
+                      handleConfigFileSubmission(files);
+                      setIsConfigDialogOpen(false);
+                    }}
+                    label={"Settings File"}
+                    description="Select a json file to load"
+                  />
+                </DialogContent>
+              </Dialog>
+              <MenubarSeparator />
+
               <MenubarItem onClick={() => reset()}>
                 {/* TODO: This should have a confirmation alert dialog */}
                 Reset
