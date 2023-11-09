3cc62f6a8201a07dfe9b15c9b506193814108286
Francisco Moretti <franciscoemoretti@gmail.com>
2023-11-09 20:37:08 +0000

    removed some imports

--- src/components/react-new-page.tsx
+++ src/components/react-new-page.tsx
@@ -1,24 +1,8 @@
-import React from "react";
-import * as z from "zod";
-import { ConfigSchema, DocumentSchema } from "@/lib/validation/document-schema";
-import { Footer } from "./react-footer";
-import { cn } from "@/lib/utils";
-import { fontIdToClassName } from "@/lib/fonts-map";
-import { OutroSlideSchema, SlideType } from "@/lib/validation/slide-schema";
+import { SlideType } from "@/lib/validation/slide-schema";
 
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Ghost, Plus, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
 import { NewSlideDialogContent } from "./new-page-dialog-content";
 
 export function NewPage({
