7f1af556ee7d92e3a1c9e05014822997b4467df8
Francisco Moretti <franciscoemoretti@gmail.com>
2023-12-05 09:44:02 +0000

    added typography components

---
+++ src/components/typography.tsx
@@ -0,0 +1,172 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export function TypographyH1({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1
+      className={cn(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyList({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+}
+
+export function TypographyH3({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+        className
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
+export function TypographyP({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}>
+      {children}
+    </p>
+  );
+}
+
+export function TypographyBlockquote({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <blockquote className={cn("mt-6 border-l-2 pl-6 italic", className)}>
+      {children}
+    </blockquote>
+  );
+}
+
+// TODO: Add table styles
+
+export function TypographyList({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", className)}>
+      {children}
+    </ul>
+  );
+}
+
+export function TypographyInlineCode({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1
+      className={cn(
+        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyLead({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <p className={cn("text-xl text-muted-foreground", className)}>{children}</p>
+  );
+}
+
+export function TypographyLarge({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cn("text-lg font-semibold", className)}>{children}</div>
+  );
+}
+
+export function TypographySmall({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <small className={cn("text-sm font-medium leading-none", className)}>
+      {children}
+    </small>
+  );
+}
+
+export function TypographyMuted({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <p className={cn("text-sm text-muted-foreground", className)}>{children}</p>
+  );
+}
