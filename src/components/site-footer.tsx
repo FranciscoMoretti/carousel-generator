e42344049e94c531349fc0142a19c369f67accee
Francisco Moretti <franciscoemoretti@gmail.com>
2024-05-13 09:43:25 +0100

    footer center

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -3,8 +3,8 @@ import { Icons } from "@/components/icons";
 export function SiteFooter() {
   return (
     <footer className="border-t border-t-secondary w-full">
-      <div className="flex container flex-col items-center justify-between gap-4 md:flex-row py-4  ">
-        <div className="flex flex-col items-center gap-4 px-2 md:px-8 md:flex-row md:gap-2 ">
+      <div className="flex container flex-col items-center justify-center gap-2 md:flex-row py-4  ">
+        <div className="flex flex-col items-center md:flex-row gap-2 ">
           <Icons.logo className="hidden md:block" />
           <p className="text-center text-xs md:text-sm leading-loose md:text-left ">
             Built by{" "}
@@ -16,20 +16,10 @@ export function SiteFooter() {
             >
               franmoretti_
             </a>
-            . Posting progress on{" "}
-            <a
-              href="https://www.linkedin.com/in/franciscomoretti/"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              LinkedIn
-            </a>
-            .
           </p>
         </div>
         <p className="text-center text-xs md:text-sm md:text-left">
-          The source code is available on{" "}
+          Code on{" "}
           <a
             href={"https://github.com/FranciscoMoretti/carousel-generator"}
             target="_blank"
